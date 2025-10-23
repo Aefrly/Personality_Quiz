45d4f6da75ff3bae73fe17aa97e9a1c5842f398b
Aefrly <fraley.allison@ic.edu>
2025-10-23 12:38:20 -0500

    Figured out my previous problem and started a function to display my result.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
+//Create object to store user answers
 let userAnswers = {}
 
-let questions = document.querySelectorAll('.question-block'); {
-    let answers = document.querySelectorAll('.answer-btn'); {
-        answers.forEach(function(button) {
-            button.addEventListener('click', function() {
-                answers.forEach(function(btn) {
-                    btn.classList.remove('selected');
-                });    
-                button.classList.add('selected');
-            })
-                let dataNum = button.dataset.num;
-                let dataAnswer = button.dataset.answer;
-                userAnswers[dataNum] = dataAnswer;
-                console.log(userAnswers);
-        }) 
-    }
+//Select all question blocks
+let questions = document.querySelectorAll('.question-block'); 
+//Loop through each question block
+questions.forEach(function(questionBlock) {
+    //Select all answer buttons within the question block
+    let answers = questionBlock.querySelectorAll('.answer-btn');
+    //Loop through each button
+    answers.forEach(function(button) {
+        button.addEventListener('click', function() {
+            answers.forEach(function(btn) {
+                btn.classList.remove('selected');
+            });
+            button.classList.add('selected');
+
+            //Store answer in the userAnswers object
+            let dataNum = button.dataset.num;
+            let dataAnswer = button.dataset.answer;
+
+            userAnswers[dataNum] = dataAnswer;
+            console.log(userAnswers);
+        });
+    });
+});    
+
+//Function to display result based on user answers
+function displayResult() {
+
 }
