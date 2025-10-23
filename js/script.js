0d39b26341a0d298582254fe3082675ca968e429
Aefrly <fraley.allison@ic.edu>
2025-10-23 12:27:27 -0500

    I keep moving things around here because I don't understand why my buttons will be highlighted for one question but removes the highlight when I click another answer on the next question.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
-console.log("script.js connected!");
+let userAnswers = {}
 
-let questions = document.querySelectorAll('question-block'); {
+let questions = document.querySelectorAll('.question-block'); {
     let answers = document.querySelectorAll('.answer-btn'); {
-        let userAnswers = {}
         answers.forEach(function(button) {
             button.addEventListener('click', function() {
                 answers.forEach(function(btn) {
                     btn.classList.remove('selected');
-                });
-            button.classList.add('selected');
+                });    
+                button.classList.add('selected');
             })
-            let dataNum = button.dataset.num;
-            let dataAnswer = button.dataset.answer;
-            userAnswers[dataNum] = dataAnswer;
-            console.log(userAnswers);
+                let dataNum = button.dataset.num;
+                let dataAnswer = button.dataset.answer;
+                userAnswers[dataNum] = dataAnswer;
+                console.log(userAnswers);
         }) 
     }
 }
